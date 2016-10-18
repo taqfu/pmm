4d61b85891932c36f6b289770b1dc26fd1fe5e5a
exnnwc <2buzezsn@gmail.com>
2016-10-18 15:33:55 -0800

    Error fix.

--- app/Confirmation.php
+++ app/Confirmation.php
@@ -8,9 +8,9 @@ class Confirmation extends Model
 {
     public static function any($message_id){
         $message = Message::find($message_id);
-        if (substring($message->confirm_period, 0, 1)==0){
+        if (substr($message->confirm_period, 0, 1)==0){
             return false;
-        } 
+        }
         var_dump(Confirmation::where('message_id', $message_id)->orderBy('created_at', 'desc')->first());
 
 
