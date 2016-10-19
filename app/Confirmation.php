5d4ab787730f13517c39334271ccb60b9db026be
taqfu <2buzezsn@gmail.com>
2016-10-19 07:24:30 -0700

    New change to evaluate

--- app/Confirmation.php
+++ app/Confirmation.php
@@ -8,10 +8,11 @@ class Confirmation extends Model
 {
     public static function any($message_id){
         $message = Message::find($message_id);
+        var_dump("ANY STARTED");
         if (substr($message->confirm_period, 0, 1)==0){
             return false;
         }
-        var_dump(Confirmation::where('message_id', $message_id)->orderBy('created_at', 'desc')->first());
+        var_dump($message_id, Confirmation::where('message_id', $message_id)->orderBy('created_at', 'desc')->first());
 
 
     }
