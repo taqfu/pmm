b8dbe9fb06c4b18b07c929fafdbd001511bc9923
taqfu <2buzesn@gmail.com>
2016-11-16 11:30:25 -0500

    Working on getting socialite to work. Done for today.

---
+++ app/SocialAccountService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Laravel\Socialite\Contracts\User as ProviderUser;
+
+
+class SocialAccountService
+{
+    public function createOrGetUser($provider)
+    {
+        $providerUser = $provider->user();
+        $providerName = class_basename($provider);
+        $account = SocialAccount::whereProvider($providerName)
+            ->whereProviderUserId($providerUser->getId())
+            ->first();
+
+        if ($account) {
+            return $account->user;
+        } else {
+
+            $account = new SocialAccount([
+                'provider_user_id' => $providerUser->getId(),
+                'provider' => $providerName,
+            ]);
+
+            $user = User::whereEmail($providerUser->getEmail())->first();
+
+            if (!$user) {
+
+                $user = User::create([
+                    'email' => $providerUser->getEmail(),
+                    'name' => $providerUser->getName(),
+                ]);
+            }
+
+            $account->user()->associate($user);
+            $account->save();
+
+            return $user;
+
+        }
+
+    }
+}
