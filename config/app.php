a978f6291ab811faa7d9ada291f894e402921375
taqfu <2buzesn@gmail.com>
2016-11-16 10:49:09 -0500

    Setuping Socialite

--- config/app.php
+++ config/app.php
@@ -178,6 +178,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Other Providers
+        Laravel\Socialite\SocialiteServiceProvider::class,
+
     ],
 
     /*
@@ -221,6 +224,7 @@ return [
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
         'Session' => Illuminate\Support\Facades\Session::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
