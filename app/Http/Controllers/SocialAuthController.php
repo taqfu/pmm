4927621b4fd4509e91b7f3c61849ca810ea8cc5b
taqfu <2buzesn@gmail.com>
2016-11-18 11:21:17 -0500

    Fully implemented social login

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -19,7 +19,7 @@ class SocialAuthController extends Controller
     public function callback(SocialAccountService $service, $provider)
     {
         $user = $service->createOrGetUser(Socialite::driver($provider));
-
         auth()->login($user);
+        return redirect(route('root'));
     }
 }
