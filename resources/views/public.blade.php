9fdbd7522cbed0c2011bdb74c80571d2011b1c7f
taqfu <2buzezsn@gmail.com>
2016-11-28 12:50:10 -0800

    Created explaining part of public page

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -1,23 +1,27 @@
 @extends ('layouts.app')
 
 @section('content')
-    
+
     <div class='text-center'>
         <img src='/logo.png' class='img-responsive center-block' />
     </div>
-    <div class='row text-center lead'>
-        <strong>Words Prevail</strong> will send out a message if something happens to you. 
+    <div id='public-primary' class='row text-center lead'>
+        <strong>Words Prevail</strong> will send out a message if something happens to you.
+        @include ('auth.login')
+        <h3 class='footer navbar-fixed-bottom text-center'>
+            <a href='#' id='replace-primary-public' class='replace-primary-button'>More</a>
+        </h3>
     </div>
-    @include ('auth.login')
-    <div class='container hidden'>
+
+    <div id='public-secondary' class='container hidden'>
+        @include ('auth.login')
         <h3 class='row'><strong>
             How It Works
         </strong></h3>
         <div class='row'>
             You check in regularly and when you fail to do so, your e-mail will be sent. If you want us to send you a reminder before that (just in case!), we can do that too.
         </div>
+        <a href='#' id='replace-secondary-public' class='replace-secondary-button'>Back</a>
     </div>
-    <h3 class='footer navbar-fixed-bottom text-center'>
-        <a href='' >More</a>
-    </h3>
+
 @endsection
