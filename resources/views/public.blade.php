9fe176411b943f099ad2895eb464d3a7328f8e33
taqfu <2buzesn@gmail.com>
2016-12-01 11:45:29 -0500

    Cleaning up site for release

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -7,25 +7,42 @@
     </div>
     <div id='public-primary' class='container text-center'>
         <div class='lead'>
-            <strong>Words Prevail</strong> will send a message if something happens to you.
+            <strong>Words Prevail</strong> will send out message if something happens to you.
         </div>
-        @include ('auth.login')
         <h3 class='footer navbar-fixed-bottom text-center'>
             <a href='#' id='replace-primary-public' class='replace-primary-button'>More</a>
         </h3>
     </div>
+        @include ('auth.login')
 
     <div id='public-secondary' class='container hidden'>
-        @include ('auth.login')
         <div >
-            <h3 class='row'><strong>
+            <h1 class='row text-center'><strong>
                 How It Works
-            </strong></h3>
-            <div class='row'>
-                You check in regularly. When you don't check in and don't respond to our follow up emails, your e-mail will be sent.
+            </strong></h1>
+            <h2>Create your message.</h2>
+            <ul>
+            <li>
+                For now, we just have e-mails available, but we're planning on expanding into other forms of communication. When you create your message, you specify how often you want to check in. Every day, every 2 days, every week, every 3 weeks, etc.
+            </li>
+            </ul>
+            <h2>Check in.</h2>
+            <div>
+            <ul>
+            <li>
+                Once your message is created, you just need to check in by logging into the site. If you don't check in, it's not a problem. We can confirm with you before sending out the message in case you don't check in.
             </div>
+            </li>
+            </ul>
+            <h2>Know your message will be sent.</h2>
+            <div >
+            <ul>
+            <li>
+                In the event that something happens to you, your message will go where it needs to.
+            </li>
+            </ul>
         </div>
-        <h3 class='footer navbar-fixed-bottom text-center'>
+        <h3 class='footer navbar navbar-fixed-bottom text-center'>
             <a href='#' id='replace-secondary-public' class='replace-secondary-button'>
                 Back
             </a>
